10254b4b2aae3b8c6cbfee5a7995a5dab61a6d76
jaeyonging <wodyd1318@naver.com>
2024-07-24 14:14:44 +0900

    check

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let service_Key = import.meta.env.VITE_SERVICE_KEY
-
+console.log(service_Key)
 export async function GetHouseData(code: string, date: string) {
     return axios.get(`/api/1613000/RTMSDataSvcOffiRent/getRTMSDataSvcOffiRent?serviceKey=${service_Key}&LAWD_CD=${code}&DEAL_YMD=${date}&numOfRows=100`).then((response) => {
         console.log(response)
