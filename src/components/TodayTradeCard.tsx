788644052a59e299ec9d343d4219cc712537a1f8
jaeyonging <wodyd1318@naver.com>
2024-07-24 15:26:13 +0900

    add analytics

--- src/components/TodayTradeCard.tsx
+++ src/components/TodayTradeCard.tsx
@@ -35,7 +35,6 @@ export const TodayTradeCard = () => {
         setTodayDate(today.getFullYear() + formattedMonth);
     }, []);
 
-
     useEffect(() => {
         if (todayDate) {
             setLoading(true)
