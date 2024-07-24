0a9bf31ccf3d246a91bb59e802ad496d12c82457
jaeyonging <wodyd1318@naver.com>
2024-07-24 16:11:06 +0900

    ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Suspense, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./routes/Home";
 import './App.css'
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = import.meta.env.VITE_GA_PROPERTYID;
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.send('pageview');
+  }, [location]);
   return (
     <Suspense fallback={<div>로딩중</div>}>
       <Routes>
