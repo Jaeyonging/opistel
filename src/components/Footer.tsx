f211630b5d757914c2971c8cb25ff161c652367a
jaeyonging <wodyd1318@naver.com>
2024-08-15 17:06:37 +0900

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,6 +23,14 @@ export const Footer = () => {
                         <a className='github' href='https://www.linkedin.com/in/jaeyong-choi-a38b86268/'> Jaeyong</a>
                     </div>
                 </div>
+                <div className='flex flex-row'>
+                    <div>
+                        Portfolio:
+                    </div>
+                    <div>
+                        <a className='github' href='https://jaeyonging.vercel.app/'> Jaeyonging</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
