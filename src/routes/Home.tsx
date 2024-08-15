e2fe0a1c38c686f4bc7aaabbae6bdb30f545ca82
jaeyonging <wodyd1318@naver.com>
2024-08-15 16:46:39 +0900

    add css

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,10 +12,10 @@ export const Home = () => {
     <>
       <div className='flex w-[100vw] flex-col'>
         <div className='flex flex-row justify-around bg-[gray] rounded-[20px] p-2 mt-2 mb-2'>
-          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px]' onClick={() => navigate("/")}>오늘의 실거래</button>
-          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px]'>매매</button>
-          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px]'>전/월세</button>
-          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px]' onClick={() => navigate("/auction")}>경매</button>
+          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px] sm:text-[10px] sm:w-[120px]' onClick={() => navigate("/")}>오늘의 실거래</button>
+          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px] sm:text-[10px] sm:w-[100px]'>매매</button>
+          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px] sm:text-[10px] sm:w-[100px]'>전/월세</button>
+          <button className='bg-[yellow] p-2 rounded-[20px] w-[150px] sm:text-[10px] sm:w-[100px]' onClick={() => navigate("/auction")}>경매</button>
         </div>
         <TodayTradeCard />
       </div>
